41befc975ca6fd0d622035f6a2f24f245f1364b5
Sean Egger <seanegger@stu-94-150-171-184.champlain.edu>
2019-04-10 15:13:54 -0400

    Initial Commit

---
+++ YourMomGenerator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  YourMomGenerator
+//
+//  Created by Sean Egger on 4/10/19.
+//  Copyright © 2019 Sean Egger. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
